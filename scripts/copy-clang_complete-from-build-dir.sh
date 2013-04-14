e9315d445e2133fd02df959ccd1f2e84358886f6
Daniel Crispell <dec@Daniels-MacBook-Pro.local>
2013-04-14 13:32:07 -0400

    give directory arg to find

--- scripts/copy-clang_complete-from-build-dir.sh
+++ scripts/copy-clang_complete-from-build-dir.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 pushd $1
-tar -cf clang_complete.tar `find  -path ./CMakeFiles -prune -o -name .clang_complete -print` 
+tar -cf clang_complete.tar `find . -path ./CMakeFiles -prune -o -name .clang_complete -print` 
 popd
 tar -xvf $1/clang_complete.tar
 
