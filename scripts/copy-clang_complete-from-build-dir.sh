b55676a381bad42084c631af60bfab24fbe7b4f7
Daniel Crispell <dancrispell@gmail.com>
2013-04-17 17:18:40 -0400

    spruced up clang_complete file copying script a bit

--- scripts/copy-clang_complete-from-build-dir.sh
+++ scripts/copy-clang_complete-from-build-dir.sh
@@ -1,7 +1,29 @@
 #!/usr/bin/env bash
+# Copy all .clang_complete files from the build tree into the source tree
+if [ -z $1 ]
+then
+  echo "usage: $0 <binary dir> [source dir (\$PWD)]"
+  exit -1
+fi
 
-pushd $1
+BINDIR=$1
+if [ -z $2 ]
+then
+  SRCDIR=$PWD
+else
+  SRCDIR=$2
+fi
+
+echo "BINDIR = $BINDIR"
+echo "SRCDIR = $SRCDIR"
+
+pushd $BINDIR
 tar -cf clang_complete.tar `find . -path ./CMakeFiles -prune -o -name .clang_complete -print` 
 popd
+
+pushd $SRCDIR
 tar -xvf $1/clang_complete.tar
+popd 
+
+echo "done."
 
