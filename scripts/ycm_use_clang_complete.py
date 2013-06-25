2ad4f437e56883fce2df62e4351dcd6b9b6157a1
Daniel Crispell <dancrispell@gmail.com>
2013-06-25 14:21:08 -0400

    traverse up filesystem tree looking for .clang_complete file

--- scripts/ycm_use_clang_complete.py
+++ scripts/ycm_use_clang_complete.py
@@ -1,4 +1,5 @@
-# Specify compiler flags for the YouCompleteMe plugin in Vim.
+""" Specify compiler flags for the YouCompleteMe plugin in Vim.
+"""
 import os.path
 
 default_flags = [
@@ -7,18 +8,29 @@ default_flags = [
     '-std=c++11',
     '-I',
     '.'
-    ]
+]
+
 
 def FlagsForFile(filename):
-    (dirname, basename) = os.path.split(filename)
-    opt_fname = os.path.join(dirname,'.clang_complete')
+    """ given the source filename, return the compiler flags """
+    opt_basename = '.clang_complete'
+    curr_dir = os.path.dirname(filename)
+    opt_fname = os.path.join(curr_dir, opt_basename)
+    # keep traversing up the tree until we find the file, or hit the root
+    while not os.path.exists(opt_fname):
+        new_dir = os.path.dirname(curr_dir)
+        if new_dir == curr_dir:
+            # we've reached the root of the tree
+            break
+        curr_dir = new_dir
+        opt_fname = os.path.join(curr_dir, opt_basename)
     try:
-        fd = open(opt_fname,'r')
+        fd = open(opt_fname, 'r')
     except IOError:
-        return {'flags': default_flags, 'do_cache' : True}
+        return {'flags': default_flags, 'do_cache': True}
     flags = [line.strip() for line in fd.readlines()]
     flags.append('-DUSE_CLANG_COMPLETER')
     return {
-      'flags': flags, 'do_cache': True
+        'flags': flags, 'do_cache': True
     }
 
